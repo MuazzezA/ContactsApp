29e2e496e7f27ca28f8423281a4f591ea138d825
MuazzezA <ayd.muazzez@gmail.com>
2023-07-26 09:28:36 +0300

    Create collection view cell

---
+++ ContactsApp/ContactDetailView/ContactCollectionView/ContactsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsCollectionViewCell.swift
+//  ContactsApp
+//
+//  Created by Muazzez Aydın on 26.07.2023.
+//
+
+import UIKit
+
+class ContactsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
